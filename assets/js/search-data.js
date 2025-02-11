6cc85b8ad0dbed4f9c674b51298eaef420deffbf
de-ranit <145925975+de-ranit@users.noreply.github.com>
2025-02-11 22:03:12 +0000

    Deploying to gh-pages from @ de-ranit/de-ranit.github.io@3c3983b538def73dde057ce35857198365439471 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Publications in reversed chronological order. (Under construction!! 👨‍💻)",
+          description: "Publications by categories in reversed chronological order.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
